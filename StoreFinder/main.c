bdb3601ef25b28383a9bb43ca458adb078ff3c13
Son SeungYeol <ssy990408@gmail.com>
2019-05-16 02:18:36 +0900

    editing struct

--- StoreFinder/main.c
+++ StoreFinder/main.c
@@ -3,14 +3,21 @@
 
 
 typedef struct {
-	int key;
-}Node;
+	int position;	 
+	char url[100];	 
+	char name[100];  
+	int price;	     
+	int type;        
+	int type_eat;    
+	//----------------
+	int near_point_dist;
+} Node;
 
+Node store[1000];
 int graph[100][100];
 
 int main()
 {
-	
 
 	system("pause");
 	return 0;
