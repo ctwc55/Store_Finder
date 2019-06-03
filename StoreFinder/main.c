88c26be4fcd95ff2240f83482f1de459f5489514
Son SeungYeol <ssy990408@gmail.com>
2019-06-03 15:05:35 +0900

    arrowkey_check

--- StoreFinder/main.c
+++ StoreFinder/main.c
@@ -1,62 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <windows.h>
 #include "storeinfo.h"
 
 #define TRUE 1
 #define FALSE 0
+#define ARROWKEY 224
+#define UP 72
+#define DOWN 80
+#define LEFT 77
+#define RIGHT 75
 
-typedef struct {
-	char url[100];	 
-	char name[100];  
-	int price;	     
-	int type;        
-	int type_eat;
-	//----------------
-	int position;
-	int store_to_pos_dist;
-	int second_near;
-	int store_to_sec_dist;
-} Node;
-
-Node store[1000];
+//Node store[1000];
 int graph[100][100];
 
 //functions
 void gotoxy(int x, int y);
-void mainPage();
+int keyReturn();
+int mainPage();
 
 int main()
 {
-	int nSelect = 0, x = 0, y = 0;
-	while (TRUE) {
+	mainPage();
+	/*while (TRUE) {
 		gotoxy(x, y);
 		printf("#");
 		if (kbhit()) {
 			nSelect = getch();
-			if (nSelect == 224) {
+			if (nSelect == ARROWKEY) {
 				nSelect = getch();
 				switch (nSelect)
 				{
-				case 72:
+				case LEFT:
 					y--;
 					break;
-				case 80:
+				case RIGHT:
 					y++;
 					break;
-				case 75:
+				case UP:
 					x--;
 					break;
-				case 77:
+				case DOWN:
 					x++;
 					break;
 				default:
 					break;
 				}
 			}
+			system("cls");
 		}
-		system("cls");
-	}
+	}*/
 
 	system("pause");
 	return 0;
@@ -68,6 +62,75 @@ void gotoxy(int x, int y)//내가 원하는 위치로 커서 이동
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);// WIN32API 함수입니다
 }
 
-void mainPage() {
+int keyReturn() {
+	int nSelect;
+	int command = 0;
+	if (kbhit()) {
+		nSelect = getch();
+		if (nSelect == ARROWKEY) {
+			nSelect = getch();
+			switch (nSelect)
+			{
+			case LEFT:
+				command = LEFT;
+				break;
+			case RIGHT:
+				command = RIGHT;
+				break;
+			case UP:
+				command = UP;
+				break;
+			case DOWN:
+				command = DOWN;
+				break;
+			default:
+				break;
+			}
+		}
+		return command;
+	}
+}
 
+int mainPage() {
+	int x=13, y=8, com;
+
+	printf("┌──────────────────────────────────────────────────────────────┐\n");
+	printf("│                    명륜동 유생의 가게 찾기 도우미                  │\n");
+	printf("├──────────────────────────────────────────────────────────────┤\n");
+	printf("│   찾고 싶은 가게 종류를 선택해주세요                                                            │\n");
+	printf("│                                                               │\n");
+	printf("│   *방향키와 엔터로 조작할 수 있습니다                                                           │\n");
+	printf("├──────────────────────────────────────────────────────────────┤\n");
+	printf("│           > 식당                │\n");
+	printf("│             약국              │\n");
+	printf("│             편의점&마트              │\n");
+	printf("│             카페              │\n");
+	printf("└──────────────────────────────────────────────────────────────┘\n");
+
+	while (TRUE) {
+		com = keyReturn();
+		switch (com)
+		{
+		case UP:
+			if (y>8) {
+				gotoxy(x, y);
+				printf(" ");
+				gotoxy(x, --y);
+				printf(">");
+			}
+			break;
+
+		case DOWN:
+			if (y < 11) {
+				gotoxy(x, y);
+				printf(" ");
+				gotoxy(x, ++y);
+				printf(">");
+			}
+			break;
+
+		default:
+			break;
+		}
+	}
 }
--- StoreFinder/storeinfo.h
+++ StoreFinder/storeinfo.h
@@ -1,1 +1,14 @@
-// 함수의 선언과 변수의 선언만 위치 
+// 함수의 선언과 변수의 선언만 위치
+
+typedef struct {
+	char url[100];
+	char name[100];
+	int price;
+	int type;
+	int type_eat;
+
+	int position;
+	int store_to_pos_dist;
+	int second_near;
+	int store_to_sec_dist;
+} Node;
